e5d8fc0be3db27dba867aaf5146915dcda789f43
reuschj <reuschj@gmail.com>
2018-02-02 01:06:06 -0600

    Test from Atom Mac

--- src/toMerge.js
+++ src/toMerge.js
@@ -1,13 +1,3 @@
-// Temp Imports ================================================================
-// =============================================================================
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-
 // utility =====================================================================
 // =============================================================================
 
